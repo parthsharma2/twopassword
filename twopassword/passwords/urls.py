774c9c8ab1c4aa51f21381cd1c5ce88aaec07892
Parth Sharma <parth261297@gmail.com>
2021-05-23 00:25:01 +0530

    add link to add a password entry in dashboard

--- twopassword/passwords/urls.py
+++ twopassword/passwords/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from twopassword.passwords import views
 
 urlpatterns = [
-    path('passwords/add', views.add_password, name="create-password"),
+    path('passwords/add', views.add_password, name="add-password"),
     path('passwords/delete/<int:password_id>', views.delete_password, name="delete-password"),
     path('', views.PasswordListView.as_view(), name="dashboard"),
 ]
